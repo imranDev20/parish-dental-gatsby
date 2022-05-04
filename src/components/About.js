7a3d73cd42c96f3750598df61bed3c1f3f1f6f96
imrandev20 <imrandev20@gmail.com>
2022-05-04 06:41:24 +0600

    Finished about

--- src/components/About.js
+++ src/components/About.js
@@ -1,35 +1,46 @@
 import React from "react";
+import Triangle from "../images/triangle.svg";
 
 const About = () => {
   return (
-    <section className="w-full bg-background">
-      <div className="w-1/3 bg-background relative max-w-lg">
-        <div className="container max-w-lg px-10 pl-20 py-20"></div>
+    <section className="bg-background">
+      <div className="w-full bg-[url('../images/operation.jpeg')] bg-[length:66%_100%] bg-no-repeat bg-[center_right]">
+        <div className="container mx-auto px-10 ">
+          <div className="w-2/5 bg-background py-28 px-20 mr-auto relative">
+            <h3 className="uppercase text-secondary font-medium tracking-[0.2em] text-sm">
+              Our dear clients
+            </h3>
+            <h2 className=" text-4xl my-2 capitalize font-semibold text-primary">
+              Fully dedicated to your dental health
+            </h2>
+            <p className="text-gray-500 font-light mx-auto my-5 text-base">
+              Te veritus tractatos delicatissimi qui, justo diceret mentitum ut
+              sit. Qui sed reque dicam, qui veri nullam vituperatoribus in.
+              Tibique maiestatis sum quod sum ut alienum nec et, summo possim
+              persequeris vix mea. Adhuc quodsi qui, sit no tale essent
+              electram. Mei sum prodesset in pro, quo scripta feugait vidisse.
+            </p>
 
-        <span className="font-['Catamaran'] text-xl font-medium text-secondary">
-          Andrea McCollins
-        </span>
+            <p className="font-['Catamaran'] text-xl text-orange-500 font-medium">
+              Andrea McCollins
+            </p>
+
+            <svg
+              className="absolute top-0 -right-28 w-28 h-full"
+              width="100%"
+              viewBox="0 0 100 700"
+              preserveAspectRatio="none"
+            >
+              <polygon
+                fill="#EEF7FF"
+                points="100 0 0 0 0 700 0 700 100 0"
+              ></polygon>
+            </svg>
+          </div>
+        </div>
       </div>
-      <div className="w-2/3"></div>
     </section>
   );
 };
 
 export default About;
-
-{
-  /* <h3 className="uppercase text-secondary font-medium tracking-[0.2em] text-sm">
-            Our dear clients
-          </h3>
-          <h2 className=" text-4xl my-2 capitalize font-semibold text-primary">
-            Fully dedicated to your dental health
-          </h2>
-
-          <p className="text-gray-500 font-light mx-auto my-5 text-base">
-            Te veritus tractatos delicatissimi qui, justo diceret mentitum ut
-            sit. Qui sed reque dicam, qui veri nullam vituperatoribus in.
-            Tibique maiestatis sum quod sum ut alienum nec et, summo possim
-            persequeris vix mea. Adhuc quodsi qui, sit no tale essent electram.
-            Mei sum prodesset in pro, quo scripta feugait vidisse.
-          </p> */
-}
