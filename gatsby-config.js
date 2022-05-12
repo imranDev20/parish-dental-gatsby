2f5976613375a7bcd03edf8c76591a33a3e63075
imrandev20 <imrandev20@gmail.com>
2022-05-13 01:59:43 +0600

    updated singletypes

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,34 +5,7 @@ require("dotenv").config({
 const strapiConfig = {
   apiURL: process.env.STRAPI_API_URL,
   accessToken: process.env.STRAPI_TOKEN,
-  collectionTypes: [
-    // {
-    //   singularName: "article",
-    //   queryParams: {
-    //     // Populate media and relations
-    //     // Make sure to not specify the fields key so the api always returns the updatedAt
-    //     publicationState:
-    //       process.env.GATSBY_IS_PREVIEW === "true" ? "preview" : "live",
-    //     populate: {
-    //       image: "*",
-    //       images: "*",
-    //       author: {
-    //         populate: {
-    //           avatar: "*",
-    //           company: {
-    //             populate: {
-    //               image: "*",
-    //             },
-    //           },
-    //         },
-    //       },
-    //     },
-    //   },
-    // },
-    // ,
-    // "company",
-    // "author",
-  ],
+  collectionTypes: [`page`],
   singleTypes: [],
 };
 
