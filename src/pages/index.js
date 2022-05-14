5fb9b97762d2a3605747f258e86d25f319f0774d
imrandev20 <imrandev20@gmail.com>
2022-05-15 05:04:49 +0600

    added loadable components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
 import * as React from "react";
-import About from "../components/About";
-import Features from "../components/Features";
-import Hero from "../components/Hero";
 import Layout from "../components/Layout";
-import Services from "../components/Services";
+import loadable from "@loadable/component";
+import Hero from "../components/Hero";
 import SpecialistsTabs from "../components/SpecialistsTabs";
 import Testimonials from "../components/Testimonials";
 import Team from "../components/Team";
@@ -11,6 +9,9 @@ import Contact from "../components/Contact";
 import Blog from "../components/Blog";
 import CallToAction from "../components/CallToAction";
 import Seo from "../components/Seo";
+const About = loadable(() => import("../components/About"));
+const Features = loadable(() => import("../components/Features"));
+const Services = loadable(() => import("../components/Services"));
 
 // markup
 const IndexPage = () => {
