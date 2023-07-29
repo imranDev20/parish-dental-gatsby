dc0e94c6325d7a79cd5ce5bdbbcf1276e60e16a9
imrandev20 <imrandev20@gmail.com>
2023-07-29 21:08:00 +0600

    fixed title problem

--- src/components/global/Seo.js
+++ src/components/global/Seo.js
@@ -27,7 +27,7 @@ function Seo({ description, lang, meta, title }) {
         lang,
       }}
       title={title}
-      titleTemplate={defaultTitle ? `${defaultTitle} | %s` : null}
+      // titleTemplate={defaultTitle ? `${defaultTitle} | %s` : null}
       meta={[
         {
           name: `description`,
