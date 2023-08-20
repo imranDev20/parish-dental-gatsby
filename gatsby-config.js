239f62d7c40ab22c8aa5ffd079881994ff5af1ab
Md. Imran Kabir <imrandev20@gmail.com>
2023-08-20 11:39:47 +0600

    added plugin netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sitemap",
     `gatsby-plugin-postcss`,
+    `gatsby-plugin-netlify`,
     {
       resolve: "gatsby-plugin-manifest",
       options: {
